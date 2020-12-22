53327aee5ab30f25aea20e4f369fc9a1aa037186
Adeel Arif Bhatti <adeelarifbhatti@gmail.com>
2020-12-22 23:40:12 +0100

    developing

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express');
+const fs = require('fs');
 
 const app = express();
-app.get('/', (req,res)=> {
+/*app.get('/', (req,res)=> {
 res.status(200).send("Hellp from server side");
 });
 
@@ -12,8 +13,19 @@ res.status(200).json({message: "Hello from server side"});
 
 app.post('/', (req,res)=> {
 res.status(200).send("You can post here");
-});
+});*/
 
+const courses = JSON.parse(fs.readFileSync(`${__dirname}/fake-data/data/tours.json`)
+	);
+app.get('/api/v1/courses',(req,res) =>{
+	res.status(200).json({
+		status: 'success',
+		results: courses.length,
+		data: {
+			courses
+		}
+	});
+});
 const port = 5000; 
 app.listen(port, () => {
 	console.log ("Server started on port 5000");
