267c0408d486b62e2b3c5bbe6fe267abc69bc131
Adeel Arif Bhatti <adeelarifbhatti@gmail.com>
2020-12-27 22:51:25 +0100

    added the code for static files i.e. public

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const app = express();
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 const courseRouter = require('./routes/courseRoutes');
 const studentRouter = require('./routes/studentRoutes');
 /*app.get('/', (req,res)=> {
