8bde4dbe4d2a991ebf4287a8a7263cb35711dde9
Adeel Arif Bhatti <adeelarifbhatti@gmail.com>
2020-12-28 00:22:29 +0100

    dotenv doesn't work. Will try to fix it later

--- src/server.js
+++ src/server.js
@@ -1,6 +1,12 @@
-const app = require('./app');
+// Don't work for some reason
+//const dotenv = require('dotenv');
+//dotenv.config({ path: './config.env' });
 
-const port = 5000; 
+const app = require('./app');
+console.log(process.env);
+// Doesn't work
+//const port = process.env.PORT ; 
+const port = 5000;
 app.listen(port, () => {
-	console.log ("Server started on port 5000");
+console.log (`Server started on port " + ${port}`);
 });
