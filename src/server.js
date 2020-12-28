263717e19ddcbcd628c55203f310d49ed12bbc4c
root <root@mol.uio.no>
2020-12-28 17:39:34 +0100

    config.env is still not working.  I have installed mongoose, editted the server.js and tested the connection with the database

--- src/server.js
+++ src/server.js
@@ -1,8 +1,23 @@
+const mongoose = require('mongoose');
+
 // Don't work for some reason
 //const dotenv = require('dotenv');
 //dotenv.config({ path: './config.env' });
 
 const app = require('./app');
+const DB = process.env.DATABASE_LOCAL
+/*const DB = process.env.DATABASE.replace('<PASSWORD>',
+	process.env.DATABASE_PASSWORD);
+	*/
+mongoose.connect('mongodb://my-mongo:27017/StudySystem', {
+	useNewUrlParser: true,
+	useCreateIndex: true,
+	useFindAndModify: false
+
+}).then(dbconnection =>{
+	console.log(dbconnection.connections);
+	console.log('Database is connected');
+});
 console.log(process.env);
 // Doesn't work
 //const port = process.env.PORT ; 
