772837cf980e77882b7081c6d3f2714ff6846c67
Adeel Arif Bhatti <adeelarifbhatti@gmail.com>
2021-01-02 17:50:58 +0100

    Installed the slugify and also wrote the code for the pre and post middleware in the model file for the course for each document instance

--- src/models/courseModel.js
+++ src/models/courseModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const courseSchema = new mongoose.Schema({
 	name: {
@@ -34,6 +35,10 @@ const courseSchema = new mongoose.Schema({
 		type: String,
 		required: [true, 'Description must be entered']
 	},
+	slug: {
+		type: String,
+
+	},
 	mainImage: {
 		type: String,
 		required: [true, 'Image must be entered']
@@ -54,6 +59,14 @@ const courseSchema = new mongoose.Schema({
 courseSchema.virtual('Weeks').get(function(){
 	return this.duration / 7;
 
+});
+courseSchema.pre('save', function(next){
+	this.slug = slugify(this.name, {lower: true});
+	next();
+});
+courseSchema.post('save', function(course, next){
+	console.log("Slug for this course is ",this.slug);
+	next();
 });
 const Course = mongoose.model('Course',courseSchema);
 /*
