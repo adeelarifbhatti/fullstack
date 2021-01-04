eecab1df4dc87d88d6d08d7a2adc372287f4faa1
root <root@mol.uio.no>
2021-01-04 15:05:13 +0100

    CastError handled

--- src/controllers/errorController.js
+++ src/controllers/errorController.js
@@ -1,33 +1,50 @@
 module.exports = (err, req, res, next)=>{
+const catchErrors = require('./../lib/catchErrors');
+const castError =(err)=>{
+	const message = `invalid ${err.path}: ${err.value}. `;
+	return new catchErrors(message,400);
+};
 
-	console.log(err.stack);
-	err.statusCode = err.statusCode || 500;
-	err.status = err.status || 'error';
-	if(process.env.NODE_ENV === 'development'){
+	const logsForProd = (err,res) => {
+		if(err.isOperational){
+			res.status(err.statusCode).json({
+			status: err.status,
+			message: err.message
+		});
+		}
+		else {
+		console.error('Error occured', err);
+		res.status(500).json({
+			status: 'error',
+			message: 'Please contact System Administrator'
+		});
+		};
+	};
+
+	const logsForDev = (err,res) => {
 		res.status(err.statusCode).json({
+		logs: "These are from my error handling",
 		status: err.status,
 		error: err,
 		message: err.message,
 		stack: err.stack
 	});
 
-	}
-	else if (process.env.NODE_ENV === 'production') {
-		if(err.isOperational){
-			res.status(err.statusCode).json({
-			status: err.status,
-			message: err.message
-		});
-	}
-	else {
-		console.error('Error occured', err);
-		res.status(500).json({
-			status: 'error',
-			message: 'Please contact System Administrator'
-		});
 	};
-	}
-	
+	console.log(err.stack);
+	err.statusCode = err.statusCode || 500;
+	err.status = err.status || 'error';
+
+if(process.env.NODE_ENV === 'development'){
+	let error = {...err};
+	if(err.name === 'CastError') err =	castError(err);
+		logsForDev(err,res);
+}
+	else if (process.env.NODE_ENV === 'production') {
+		let error = {...err};
+		if(err.name === 'CastError') err =	castError(err);
+		logsForProd(err,res);
+}
 
 
 
