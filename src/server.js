c341abc619cfa872eda184a00674a3a82794c419
root <root@mol.uio.no>
2021-01-05 10:55:44 +0100

    Added code for uncaughtException

--- src/server.js
+++ src/server.js
@@ -4,7 +4,11 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({ path: `${__dirname}/config.env` });
 const app = require('./app');
-
+process.on('uncaughtException', err=>{
+	console.log(err.name,err.message);
+	console.log('Uncaught Exception');
+			process.exit(1);
+});
 
 /*const DB = process.env.DATABASE.replace('<PASSWORD>',
 	process.env.DATABASE_PASSWORD);
