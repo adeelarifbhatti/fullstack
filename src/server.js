bc72b736b977f53d6316003f82ed21cacb62f598
root <root@mol.uio.no>
2021-01-05 09:42:26 +0100

    Added the code for unhandledRejection

--- src/server.js
+++ src/server.js
@@ -24,6 +24,14 @@ console.log("VARIABLE IN config.env VARIABLE ", process.env.VARIABLE);
 // Doesn't work
 //const port = process.env.PORT ;
 const port = process.env.PORT;
-app.listen(port, () => {
+const server = app.listen(port, () => {
 console.log (`Server started on port " + ${port}`);
+});
+
+process.on('unhandledRejection', err=>{
+	console.log(err.name,err.message);
+	console.log('UNHANDLED REJECTION');
+	server.close(() =>{
+		process.exit(1);
+	});
 });
