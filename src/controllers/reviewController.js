a60dbd040b986e43dcef7f90a7d82eeb1bd497fa
root <root@mol.uio.no>
2021-01-19 09:33:52 +0100

    Using the courseID in the /courses/courseID/reviews and getting the reviews of the course. It also made the use of mergeParams

--- src/controllers/reviewController.js
+++ src/controllers/reviewController.js
@@ -3,7 +3,10 @@ const Review = require('./../models/reviewModel');
 const appErrors = require('./../lib/appErrors');
 
 exports.getAllReviews = tryCatch(async(req,res,next) => {
-    const reviews = await Review.find();
+        // thanks to mergeParams
+    let filter = {};
+    if(req.body.courseId) filter = {course: req.params.courseId};
+    const reviews = await Review.find(filter);
     res.status(200).json({
         status: 'success',
         results: reviews.length,
@@ -14,6 +17,7 @@ exports.getAllReviews = tryCatch(async(req,res,next) => {
 
 });
 exports.createReview = tryCatch(async(req,res,next)=>{
+    // thanks to mergeParams
     //if these parameters are not in the body then initializing them
     if(!req.body.course) req.body.course = req.params.courseId;
     if(!req.body.user) req.body.user = req.user.id;
