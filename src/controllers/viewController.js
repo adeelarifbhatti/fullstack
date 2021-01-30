26d4e657e1947dc6a74e69e41aa2aadc5f1c02f2
Adeel Arif Bhatti <adeelarifbhatti@gmail.com>
2021-01-30 13:16:14 +0100

    Courses are working, need to work alot on layout

--- src/controllers/viewController.js
+++ src/controllers/viewController.js
@@ -1,5 +1,22 @@
+const Course = require('../models/courseModel');
+const tryCatch = require('../lib/tryCatch');
 exports.getLogin = (req,res)=> {
 	res.status(200).render('login', {
         title: 'Adeel Please Login'
     });
 }
+
+exports.getOverview = tryCatch(async(req,res,next)=> {
+    // get courses from collection
+    const courses = await Course.find();
+    console.log("From viewController getOverview ",courses);
+    // Build Template
+	res.status(200).render('overview', {
+        title: 'Overview',
+        courses
+    });
+   });
+
+
+
+
--- src/routes/viewRoutes.js
+++ src/routes/viewRoutes.js
@@ -5,4 +5,6 @@ const router = express.Router();
 
 router.get('/', viewController.getLogin);
 
+router.get('/overview', viewController.getOverview);
+
 module.exports = router;
