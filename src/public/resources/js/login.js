18035d133a842f5978c9411da10ebf259de90ff2
root <root@mol.uio.no>
2021-02-10 13:20:08 +0100

    Installed percel-bundler, axios and babel/polyfill. Added watch:js and build:js scripts in package.json, made changes in docker-compose.yml so that it mounts package.json in container. Moved the JS code from login.js to index.js and imported/exported the required functions. Also included the index.js file in the parent.pug which is created by the percel-bundler.

--- src/public/resources/js/login.js
+++ src/public/resources/js/login.js
@@ -1,4 +1,6 @@
-const login = async(email,password) => {
+import '@babel/polyfill'
+import axios from 'axios';
+export const login = async(email,password) => {
     try{
         const res = await axios({
             method: 'POST',
@@ -21,9 +23,3 @@ const login = async(email,password) => {
 
 };
 
-document.querySelector('.form1').addEventListener('submit',e => {
-    e.preventDefault();
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
-    login(email,password);
-});
